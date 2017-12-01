e5b36c312ec98f45f5062672c38ff3b97bc2c7a1
Takehiro1120 <g1344496@cse.kyoto-su.ac.jp>
2017-12-01 19:29:26 +0900

    UpUnityProject_2

---
+++ Inu_to_Ohagi/Assets/Scripts/Title/TitleSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleSystem : Singleton<TitleSystem> {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update(){
+		if (Input.GetKey("escape"))
+			Application.Quit();
+	}
+
+	public void StartGame(){
+		SceneManager.LoadScene("Setting");
+	}
+
+	public void FinishGame(){
+		Application.Quit ();
+	}
+
+}
