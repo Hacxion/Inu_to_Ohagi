5abdb185d55b7119ee5a4e7247c2412ab06c7718
Takehiro1120 <g1344496@cse.kyoto-su.ac.jp>
2017-12-04 01:38:35 +0900

    設定仮3

--- Inu_to_Ohagi/Assets/Scripts/Counter.cs
+++ Inu_to_Ohagi/Assets/Scripts/Counter.cs
@@ -1,15 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class Counter : MonoBehaviour {
+public class CounterUI : MonoBehaviour {
+
+	[SerializeField] int firstNum;
+	[SerializeField] int min;
+	[SerializeField] int max;
+	private int num;
+	[SerializeField] Text numberText;
+	[SerializeField] Button downButton;
+	[SerializeField] Button upButton;
+	private int preNum;
 
 	// Use this for initialization
 	void Start () {
-	
+		num = firstNum;
+		preNum = firstNum;
+		CounterUpdate ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update(){
+		if (preNum != num)
+			CounterUpdate ();
 	}
+
+	public void Up(){
+		num++;
+	}
+
+	public void Down(){
+		num--;
+	}
+
+	public int GetNum(){
+		return num;
+	}
+
+	public void SetNum(int num){
+		this.num = num;
+	}
+
+	public void ReSetNum(){
+		this.num = firstNum;
+	}
+
+	public void CounterUpdate(){
+		preNum = num;
+		numberText.text = "" + num;
+		if (num >= max)
+			upButton.interactable = false;
+		else
+			upButton.interactable = true;
+		if (num <= min)
+			downButton.interactable = false;
+		else
+			downButton.interactable = true;
+	}
+		
 }
